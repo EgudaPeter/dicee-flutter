0ef8051f9c0c5153f957343b8d091ec82e4393cb
Peter Eguda <peguda@teamapt.com>
2020-01-15 14:41:25 +0100

    Genesis

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.green,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.green,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  Random random = new Random();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = random.nextInt(6);
+                  leftDiceNumber = leftDiceNumber == 0 ? 1 : leftDiceNumber;
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+              onPressed: () {
+                setState(() {
+                  rightDiceNumber = random.nextInt(6);
+                  rightDiceNumber = rightDiceNumber == 0 ? 1 : rightDiceNumber;
+                });
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
